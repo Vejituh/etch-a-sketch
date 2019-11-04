ea3c634662682120f3e606fe8502f5a359119ca4
Rafael Gomes <rafaelg1402@protonmail.com>
2019-11-04 16:26:50 +0000

    added grid layout and logic to JS file

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
+const btnClear = document.querySelector(".clear");
+const container = document.querySelector(".container");
+const gridContainer = document.querySelector("#grids");
+let gridNum = 16;
 const createGrids = () => {
-    for (let i =0; i < 256; i++){
+    const gridContainerWidth = document.getElementById("grids").offsetWidth
+    const divWidth = gridContainerWidth / gridNum;
+    
+    for (let i =0; i < gridNum * gridNum; i++){
         let div = document.createElement("div");
-        div.style.background = "black";
-        div.style.height = "10px";
-        div.style.width = "10px";
-        div.style.padding = "5px";
-        div.style.border = "solid white 1px";
+        div.style.background = "#14145b";
+        div.style.border = "solid #f5f5dc 0.1px";
         
-        document.querySelector(".grids").appendChild(div);
+        gridContainer.appendChild(div);
     }
+    
+    gridContainer.style.gridTemplateRows = `repeat(${gridNum}, ${divWidth}px [row-start]`;
+    gridContainer.style.gridTemplateColumns = `repeat(${gridNum}, ${divWidth}px [column-start]`;  
+    const divs = document.querySelectorAll( '#grids div');
+    
+    divs.forEach((div) => {
+        let hover = () => {
+            div.style.background = "#707019";
+        }
+        div.addEventListener("mouseenter", hover);    
+    });
 }
+
+const clearGrid = () => {
+    gridNum = prompt("What size do you want the grid to be?");
+    while (gridContainer.hasChildNodes()) {
+    gridContainer.removeChild(gridContainer.lastChild);
+}
+    createGrids();
+}
+
+
+btnClear.addEventListener("click", clearGrid);
